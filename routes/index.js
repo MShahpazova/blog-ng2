baefdedbbd104d71d4544cd06a6b5f1e1ae201c7
nshahpazov <nshahpazov@gmail.com>
2016-12-24 13:12:29 +0200

    initial project setup

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+'use strict';
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const authentication = require('./authentication');
+const users = require('./users');
+const posts = require('./posts');
+const comments = require('./comments');
+
+router.all('/api/*', (req, res, next) => _.has(req, 'session.user') ? next() : res.sendStatus(401));
+router.use('/authentication', authentication);
+router.use('/api/users', users);
+router.use('/api/posts', posts);
+router.use('/api/comments', comments);
+
+module.exports = router;

6bbc43c1d8a319e3b60128ffc2370d43a2169629
MShahpazova <mshahpazova@gmail.com>
2016-12-31 18:53:29 +0200

    Created users and posts

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ const users = require('./users');
 const posts = require('./posts');
 const comments = require('./comments');
 
+router.use('/example', (req, res) => res.json({message: 'Hello there!'}));
+
 router.all('/api/*', (req, res, next) => _.has(req, 'session.user') ? next() : res.sendStatus(401));
 router.use('/authentication', authentication);
 router.use('/api/users', users);
